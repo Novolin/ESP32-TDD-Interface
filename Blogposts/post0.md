20cb3796b4a6faa41be6820710e2601c4823b5a8
Captain Novolin <captainnovolin@gmail.com>
2025-12-09 16:25:49 -0500

    Working Encode/Decode! Dropped blog stuff from github.

--- Blogposts/post0.md
+++
@@ -1,5 +0,0 @@
-# Part 1: Beginnings
-
-As with all good things, we start somewhere. I've whipped up a quick file with some basic Baudot functions and arrays:
-[img - screenshot of baudot.h]
-
--- baudot/baudot.h
+++ baudot/baudot.h
@@ -1,13 +1,23 @@
-// My first attempt at a header file
+// My first attempt at baudot decoding/encoding
 // Please don't hate me!!
 
 #ifndef BAUDOT_LIB
 #define BAUDOT_LIB
 
+
+// Shortcuts for non-printing baudot codes:
+#define BD_NULLCHAR 0
+#define BD_LTRSSWITCH 31
+#define BD_FIGSSWITCH 27
+
+enum IO_TARGETS{TDD_RX, TDD_TX, CPU_RX, CPU_TX};
+
+
+enum BD_MODES{BD_LTRS, BD_FIGS, BD_NONE}; 
 // Character sets:
 
 const unsigned char LTRS[32] = {
-    253,
+    BD_NULLCHAR,
     'E',
     '\n',
     'A',
@@ -34,81 +44,106 @@ const unsigned char LTRS[32] = {
     'O',
     'B',
     'G',
-    255,
+    BD_FIGSSWITCH,
     'M',
     'X',
     'V',
-    254
+    BD_LTRSSWITCH
 };
 
-/*
-typedef enum LTRS {NULCHR, 
-    BD_E, 
-    BD_LF, 
-    BD_A, 
-    BD_SP,
-    BD_S,
-    BD_I, 
-    BD_U, 
-    BD_CR, 
-    BD_D, 
-    BD_R, 
-    BD_J, 
-    BD_N, 
-    BD_F, 
-    BD_C, 
-    BD_K, 
-    BD_T, 
-    BD_Z, 
-    BD_L, 
-    BD_W, 
-    BD_H, 
-    BD_Y, 
-    BD_P, 
-    BD_Q, 
-    BD_O, 
-    BD_B, 
-    BD_G, 
-    BD_FIGS, 
-    BD_M, 
-    BD_X, 
-    BD_V, 
-    BD_LTRS
+const unsigned char FIGS[32] = {
+    BD_NULLCHAR,
+    '3',
+    '\n',
+    '-',
+    ' ',
+    '\b',
+    '8',
+    '7',
+    '\r',
+    '$',
+    '4',
+    '\'',
+    ',',
+    '!',
+    ':',
+    '(',
+    '5',
+    '\\',
+    ')',
+    '2',
+    '#',
+    '6',
+    '0',
+    '1',
+    '9',
+    '?',
+    '&',
+    BD_FIGSSWITCH,
+    '.',
+    '/',
+    ';',
+    BD_LTRSSWITCH
+
 };
-typedef enum FIGS {BF_NULCHR, 
-    BD_3, 
-    BF_LF, 
-    BD_DASH, 
-    BF_SP, 
-    BD_BELL, 
-    BD_8, 
-    BD_7, 
-    BF_CR, 
-    BD_DOLLAR, 
-    BD_4, 
-    BD_SQUOTE, 
-    BD_COMMA, 
-    BD_EXCLAIM, 
-    BD_COLON, 
-    BD_LEFTPAR, 
-    BD_5, 
-    BD_BSLASH, 
-    BD_RIGHTPAR, 
-    BD_2, 
-    BD_POUND, 
-    BD_6, 
-    BD_0, 
-    BD_1, 
-    BD_9,
-    BD_QMARK,
-    BD_AMP,
-    BF_FIGS,
-    BD_FSLASH,
-    BD_SEMICOL,
-    BF_LTRS
-};*/
+
+
+/* commonly used lookup functions */
+
+unsigned char bd_char_lookup(char target_char);
+unsigned char bd_char_mode(char target);
+
+
+unsigned char bd_char_lookup(char target_char){
+    // Finds where a character's index is
+    // First ensure it's capitalized:
+    if (target_char > 0x60 && target_char < 0x7B){
+        target_char = target_char - 0x20; // capitals are 0x20 positions lower on the ascii table
+    }
+    unsigned char searchindex = 0;
+    while (searchindex < 32){   // only 32 possible characters in our set:
+        if (LTRS[searchindex] == target_char){
+            return searchindex;
+        } else if (FIGS[searchindex] == target_char){
+            return searchindex;
+        }
+        searchindex++;
+    }   
+    return BD_NULLCHAR; // Not a valid character!
+}
+
+unsigned char bd_char_mode(char target){
+    /* returns the baudot set that contains the target character */
+    // First ensure it's capitalized:
+    if (target > 0x60 && target < 0x7B){
+        target = target - 0x20; // capitals are 0x20 positions lower on the ascii table
+    }
+
+    unsigned char searchindex = 0;
+    while (searchindex < 32){
+        if (LTRS[searchindex] == target){
+            return BD_LTRS;
+        } else if (FIGS[searchindex] == target){
+            return BD_FIGS;
+        }
+        searchindex++;
+    }
+    return BD_NONE; // not a valid character :(
+}
+
+unsigned char bd_get_char(char target_char, char character_set){
+    /* Returns the character from the given character set*/
+    unsigned char output;
+    if (character_set == BD_FIGS){
+        output = FIGS[target_char];
+    } else if (character_set == BD_LTRS){
+        output = LTRS[target_char];
+    } else {
+        output = '\0'; // Null character!!
+    }
+    return output;
+}
 
 
 
-
-#endif // EoF
+#endif
